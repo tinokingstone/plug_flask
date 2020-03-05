35ec1c8a55049fc317ca4029aefb4b405e69b807
tinokingstone <60784004+tinokingstone@users.noreply.github.com>
2020-03-05 15:38:11 +0000

    Update __init__.py

--- plug_app/__init__.py
+++ plug_app/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "6424b0e72ff44c32600b68d0c9f9a3e5" #CHANGE BEFORE LAUNCH
-app.config['SQLALCHEMY_DATABASE_URI'] ="mysql+pymysql://root:@127.0.0.1/plug" #CHANGE BEFORE LAUNCH
+app.config['SQLALCHEMY_DATABASE_URI'] ="mysql+pymysql://tinokingstone:password@127.0.0.1/plug" #CHANGE BEFORE LAUNCH
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
